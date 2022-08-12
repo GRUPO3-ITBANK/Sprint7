6cd9e859946bb2f4ea2970dfe378e90073e1c0e9
Exequiel <Exequiel.seltzer@gmail.com>
2022-08-12 00:22:07 -0300

    Co-authored-by: Lucas Segura <Glow-S@users.noreply.github.com> Co-authored-by: Nadina Jauch <NadinaJauch@users.noreply.github.com>

---
+++ homebanking/ITBA/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    return render(request, "ITBA/index.html")
+
+def home(request):
+    return render(request, "ITBA/home.html")
+    
