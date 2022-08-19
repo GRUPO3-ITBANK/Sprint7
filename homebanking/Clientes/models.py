1492f533cef4069187a74d5e7549ca2345b085c5
Nuchi <nadinajauch@gmail.com>
2022-08-19 14:02:16 -0300

    Terminado relacion usuario cliente 1:1

--- homebanking/Clientes/models.py
+++ homebanking/Clientes/models.py
@@ -1,8 +1,4 @@
 from django.db import models
-
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-
 class Cliente(models.Model):
     
     email = models.EmailField(verbose_name='mail address',max_length=255)
