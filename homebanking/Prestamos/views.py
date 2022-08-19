84d730db506a87c73f9ae524ed6d8e2870c1818d
Nuchi <nadinajauch@gmail.com>
2022-08-19 16:46:40 -0300

    pagina de prestamos

--- homebanking/Prestamos/views.py
+++ homebanking/Prestamos/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def prestamos(request):
+    if not (request.user.is_authenticated):
+        return render(request, 'ITBA/index.html')
+
+    return render(request, 'Prestamos/prestamos.html')
--- homebanking/homebanking/urls.py
+++ homebanking/homebanking/urls.py
@@ -15,8 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-import registration, ITBA, Clientes
-from Clientes import views
+import registration, ITBA,  Prestamos
+from Prestamos import views
 from ITBA import views
 from registration import views
 
@@ -27,5 +27,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('logout-session/', registration.views.logout, name='logout-session'),
     path('accounts/',include('django.contrib.auth.urls')),
+    path('prestamos/', Prestamos.views.prestamos, name="prestamos"),
     # path('alta-clientes/', Clientes.views.alta_cliente, name="alta_cliente")
 ]
